IllusiveMan196/wrye-bash

commit 8977e43a5238b57b15ff00a6fb990e303428cc9c

FO3, FNV: Port Tweak Names
Brings all the tweaks present in valda's version into our version. With
this, we are finally at feature parity.
Nice side effect is that you can now choose a custom format for each of
the tweaks. This was impossible to implement pre-tweak-pooling because
we lacked a way to validate that the format will work, but TP gave us
validate_values.

Closes #468

Co-authored-by: valda <valda@underscore.jp>

Mopy/bash/patcher/patchers/multitweak_settings.py
@@ -170,7 +170,7 @@ class _AGmstTweak(MultiTweakItem):
                     return _(u"The value chosen for GMST '%s' must be a "
                              u'float, but is currently of type %s (%s).') % (
                         target_eid, type(target_value).__name__, target_value)
-        return None
+        return super(_AGmstTweak, self).validate_values(chosen_values)
 
     def wants_record(self, record):
         if record.fid[0] not in bush.game.bethDataFiles:

Mopy/Docs/Wrye Bash General Readme.html
@@ -1038,7 +1038,12 @@ Just like the game's masters, this load order is hardcoded, and so it cannot be 
 </table>
 
 <h3 id="patch-tweaking">Applying Tweaks <a class="back2top" href="#contents">Back to top</a></h3>
-<p>The Bashed Patch lets you independently tweak a large number of game settings. Each tweak has a description that is visible when you select it. Some tweaks have several possible values, with the current value given in [square brackets] in the tweak name. Right-clicking these tweaks will display a menu from which you can select a different value. The tweaks are grouped into different sections:</p>
+<p>The Bashed Patch lets you independently tweak a large number of game
+    settings. Each tweak has a description that is visible when you select it.
+    Some tweaks have several possible values, with the current value given in
+    [square brackets] in the tweak name. Right-clicking these tweaks will
+    display a menu from which you can select a different value. The tweaks are
+    grouped into different sections:</p>
 <table>
     <thead>
         <tr>
@@ -1063,14 +1068,18 @@ Just like the game's masters, this load order is hardcoded, and so it cannot be 
                 <strong>Nvidia Fog Fix</strong> are recommended for all users,
                 as they fix bugs that may be uncovered by mods. Also in
                 Oblivion, the <strong>DarNified Books</strong> option is
-                recommended for users of DarNified UI. Beyond that, the
-                decision is entirely based on user preference.</td>
+                recommended for users of DarNified UI. In Skyrim and games
+                based on it, the <strong>Magic: Summoning Absorption
+                    Fix</strong> is recommended for all users as it fixes an
+                issue that affects many mods. Beyond that, the decision is
+                entirely based on user preference.</td>
             <td>All but Fallout 4</td>
         </tr>
-        <tr><td id="patchTweakClothes">Tweak Clothes</td>
+        <tr>
+            <td id="patchTweakClothes">Tweak Clothes</td>
             <td>Contains a set of tweaks that change settings relating to the
-                game's clothing.<td>
-                The decision is entirely based on user preference.</td>
+                game's clothing.</td>
+            <td>The decision is entirely based on user preference.</td>
             <td>Oblivion &amp; Nehrim&colon; At Fate's Edge</td>
         </tr>
         <tr>
@@ -1079,12 +1088,13 @@ Just like the game's masters, this load order is hardcoded, and so it cannot be 
                 the game. Most change the names to change how they are sorted
                 in your inventory.</td>
             <td>The decision is entirely based on user preference.</td>
-            <td>Oblivion &amp; Nehrim&colon; At Fate's Edge</td>
+            <td>Oblivion, Nehrim&colon; At Fate's Edge, Fallout 3 &amp;
+                Fallout&colon; New Vegas</td>
         </tr>
         <tr><td id="patchTweakRaces">Tweak Races</td>
             <td>Contains a set of tweaks that alter race-related things (e.g.
-                height, eyes, hairs, etc.).</td><td>The decision is entirely
-                based on user preference.</td>
+                height, eyes, hairs, etc.).</td>
+            <td>The decision is entirely based on user preference.</td>
             <td>All but Fallout 4</td>
         </tr>
         <tr>

Mopy/bash/basher/gui_patchers.py
@@ -739,9 +739,9 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
                             _(u'The values you entered (%s) are not valid '
                               u'for this tweak.') % u', '.join(
                                 unicode(s) for s in value))
-            balt.showError(self.gConfigPanel, error_header + u'\n\n' +
-                           _(u'Reason: %s') % validation_error,
-                           title=tweak.tweak_name + _(u' - Error'))
+            balt.showError(self.gConfigPanel,
+                           error_header + u'\n\n' + validation_error,
+                           title=_(u'%s - Error') % tweak.tweak_name)
 
     def mass_select(self, select=True):
         """'Select All' or 'Deselect All' button was pressed, update all
@@ -1265,8 +1265,8 @@ class TweakSettings(_GmstTweakerPanel):
 # -----------------------------------------------------------------------------
 class TweakNames(_TweakPatcherPanel):
     patcher_name = _(u'Tweak Names')
-    patcher_desc = _(u'Tweak object names in various ways such as lore '
-                     u'friendliness or show type/quality.')
+    patcher_desc = _(u'Tweak object names to sort them by type/stats or to '
+                     u'improve things like lore friendliness.')
     _config_key = u'NamesTweaker'
     patcher_type = multitweak_names.TweakNamesPatcher
 

Mopy/bash/game/fallout3/__init__.py
@@ -146,8 +146,6 @@ class Fallout3GameInfo(PatchGame):
 
     allTags = PatchGame.allTags | {u'NoMerge'}
 
-    # Remaining to add (some of these might be impossible to port):
-    #  TweakNames, ReplaceFormIDs
     patchers = {
         u'AliasModNames', u'ContentsChecker', u'FormIDLists', u'ImportActors',
         u'ImportActorsAIPackages', u'ImportActorsAnimations', u'NpcChecker',
@@ -159,7 +157,7 @@ class Fallout3GameInfo(PatchGame):
         u'ImportScripts', u'ImportSounds', u'ImportSpellStats', u'ImportStats',
         u'ImportText', u'LeveledLists', u'MergePatches', u'TweakActors',
         u'TweakAssorted', u'TweakSettings', u'ImportRacesRelations',
-        u'TweakRaces', u'TimescaleChecker',
+        u'TweakRaces', u'TimescaleChecker', u'TweakNames',
     }
 
     weaponTypes = (

Mopy/bash/game/fallout3/patcher/__init__.py
@@ -651,7 +651,14 @@ actor_tweaks = {
 #------------------------------------------------------------------------------
 # Tweak Names
 #------------------------------------------------------------------------------
-body_tags = u'HAGPBFE'
+names_tweaks = {
+    u'NamesTweak_BodyPartCodes',
+    u'NamesTweak_Body_Armor_Fo3',
+    u'NamesTweak_Ingestibles_Fo3',
+    u'NamesTweak_Weapons_Fo3',
+    u'NamesTweak_AmmoWeight_Fo3',
+}
+body_part_codes = (u'HAGPBFE', u'HBGPEFE')
 
 #------------------------------------------------------------------------------
 # Tweak Settings
@@ -770,11 +777,11 @@ import_races_attrs = {
 # Tweak Races
 #------------------------------------------------------------------------------
 race_tweaks = {
-    u'RaceTweak_PlayableEyes',
+    u'RaceTweak_PlayableHeadParts',
     u'RaceTweak_MergeSimilarRaceHairs',
     u'RaceTweak_MergeSimilarRaceEyes',
+    u'RaceTweak_PlayableEyes',
     u'RaceTweak_PlayableHairs',
-    u'RaceTweak_PlayableHeadParts',
     u'RaceTweak_GenderlessHairs',
     u'RaceTweak_AllEyes',
     u'RaceTweak_AllHairs',

Mopy/bash/game/fallout3/records.py
@@ -125,7 +125,7 @@ class MelBipedData(MelStruct):
     _biped_flags = BipedFlags()
     _general_flags = Flags(0, Flags.getNames(
         fnv_only((2, u'hasBackpack')),
-        fnv_only((3, u'medium')),
+        fnv_only((3, u'medium_armor')),
         (5, u'powerArmor'),
         (6, u'notPlayable'),
         (7, u'heavyArmor'),
@@ -207,6 +207,21 @@ class MelEmbeddedScript(MelSequential):
 class MelEquipmentType(MelSInt32):
     """Handles the common ETYP subrecord."""
     def __init__(self):
+        ##: On py3, we really need enums for records. This is a prime candidate
+        # 00: 'Big Guns',
+        # 01: 'Energy Weapons',
+        # 02: 'Small Guns',
+        # 03: 'Melee Weapons',
+        # 04: 'Unarmed Weapon',
+        # 05: 'Thrown Weapons',
+        # 06: 'Mine',
+        # 07: 'Body Wear',
+        # 08: 'Head Wear',
+        # 09: 'Hand Wear',
+        # 10: 'Chems',
+        # 11: 'Stimpak',
+        # 12: 'Food',
+        # 13: 'Alcohol'
         super(MelEquipmentType, self).__init__(b'ETYP', u'equipment_type', -1)
 
 #------------------------------------------------------------------------------

commit e17b450aabeba4d165b8ee753b3bb1bb95b0505c

Fix regression in names tweaks logging
Broke in 8977e43a5238b57b15ff00a6fb990e303428cc9c.
Also check that tweak_log_msg is not OVERRIDE at BP init to catch errors
like this in the future.

Mopy/bash/patcher/base.py
@@ -196,7 +196,8 @@ class AMultiTweakItem(object):
 
     def __init__(self):
         # Don't check tweak_log_msg, settings tweaks don't use it
-        for tweak_attr in (u'tweak_name', u'tweak_tip', u'tweak_key'):
+        for tweak_attr in (u'tweak_name', u'tweak_tip', u'tweak_key',
+                           u'tweak_log_msg'):
             if getattr(self, tweak_attr) == u'OVERRIDE':
                 self._raise_tweak_syntax_error(u"A '%s' attribute is still "
                                                u'set to the default '

Mopy/bash/patcher/patchers/multitweak_names.py
@@ -43,7 +43,6 @@ _ignored_chars = frozenset(u'+-=.()[]<>')
 
 class _ANamesTweak(CustomChoiceTweak):
     """Shared code of names tweaks."""
-    tweak_log_msg = _(u'Items Renamed: %(total_changed)d')
     _tweak_mgef_hostiles = set()
     _tweak_mgef_school = {}
     _choice_formats = [] # The default formats for this tweak
@@ -183,6 +182,7 @@ class NamesTweak_BodyPartCodes(CustomChoiceTweak): # loads no records
     tweak_name = _(u'Body Part Codes')
     tweak_tip = _(u'Sets body part codes used by Armor/Clothes name tweaks.')
     tweak_key = u'bodyTags'
+    tweak_log_msg = u'' # we log nothing
     tweak_choices = [(c, c) for c in bush.game.body_part_codes]
     tweak_order = 9 # Run before all other tweaks
 
@@ -569,6 +569,7 @@ class _ANamesTweak_AmmoWeight(_ANamesTweak):
     tweak_tip = _(u'Appends the weight of ammunition to the end of the '
                   u'ammunition name.')
     tweak_key = u'AmmoWeight'
+    tweak_log_msg = _(u'Ammunition Renamed: %(total_changed)d')
     _choice_formats = [u' (WG %s)', u' (%s)']
     _example_item = _(u'Iron Arrow')
     _example_code = u'0.01'
@@ -667,6 +668,7 @@ class _ATextReplacer(MultiTweakItem):
         b'WEAP': (u'full',),
     }
     tweak_read_classes = tuple(_match_replace_rpaths)
+    tweak_log_msg = _(u'Items Renamed: %(total_changed)d')
     # Will be passed to OrderedDict to construct a dict that maps regexes we
     # want to match to replacement strings. Those replacements will be passed
     # to re.sub, so they will apply in order and may use the results of their

Mopy/bash/patcher/patchers/multitweak_settings.py
@@ -28,10 +28,14 @@ from itertools import izip
 from .base import MultiTweakItem, MultiTweaker, CustomChoiceTweak
 from ... import bush  # for game
 
-class _AGlobalsTweak(CustomChoiceTweak):
+class _ASettingsTweak(MultiTweakItem):
+    """Shared code of GLOB and GMST tweaks."""
+    tweak_log_msg = u'' # not logged for GMST tweaks
+    show_key_for_custom = True
+
+class _AGlobalsTweak(_ASettingsTweak, CustomChoiceTweak):
     """Sets a global to specified value."""
     tweak_read_classes = b'GLOB',
-    show_key_for_custom = True
 
     @property
     def chosen_value(self):
@@ -121,10 +125,9 @@ class GlobalsTweak_Crime_ForceJail(_AGlobalsTweak):
     default_choice = u'5000'
 
 #------------------------------------------------------------------------------
-class _AGmstTweak(MultiTweakItem):
+class _AGmstTweak(_ASettingsTweak):
     """Sets a GMST to specified value."""
     tweak_read_classes = b'GMST',
-    show_key_for_custom = True
 
     @property
     def chosen_eids(self):